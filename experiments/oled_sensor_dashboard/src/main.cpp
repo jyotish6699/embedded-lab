6bbe8c9d641024aee736fe96a0e9bf631b7640e3
jyotish kumar <jyotishkumar725015@gmail.com>
2026-06-06 00:07:00 +0530

    feat(dashboard): build realtime OLED dashboard with HC-SR04 and IR sensor

--- experiments/oled_sensor_dashboard/src/main.cpp
+++ experiments/oled_sensor_dashboard/src/main.cpp
@@ -6,6 +6,10 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define IR_PIN 2
+
 // create display object to control display
 Adafruit_SSD1306 display (
     SCREEN_WIDTH,
@@ -14,30 +18,98 @@ Adafruit_SSD1306 display (
     -1       // no reset button
 );
 
+float getDistance() {
 
-void setup() {
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
 
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // start communication
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
 
+    long duration = pulseIn(ECHO_PIN, HIGH);
+
+    float distance = duration * 0.034 / 2.0;
+
+    return distance;
+}
+
+bool isObjectDetected() {
+
+    return digitalRead(IR_PIN) == LOW;
+}
+
+void drawDashboard(
+    float distance,
+    bool irDetected,
+    String state
+) {
     display.clearDisplay();
 
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
 
     display.setCursor(0, 0);
-    display.println("DIST : 0 cm");
+    display.print("DIST : ");
+    display.print(distance, 1);
+    display.println(" cm");
 
     display.setCursor(0, 20);
-    display.println("IR   : CLEAR");
+    display.print("IR   : ");
+
+    if (irDetected) {
+        
+        display.println("DETECTED");
+    }else {
+        
+        display.println("CLEAR");
+    }
 
     display.setCursor(0, 40);
-    display.println("STATE: SAFE");
+    display.print("STATE: ");
+    display.println(state);
 
-    display.display();   // send everthing(RAM buffer) to screen NOW
+    display.display();
+}
+
+
+void setup() {
+
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+
+    pinMode(IR_PIN, INPUT);
+
+    display.begin(
+        SSD1306_SWITCHCAPVCC,
+        0x3C
+    );
+
+    display.clearDisplay();
+    display.display();
 }
 
 void loop() {
 
-}
+    float distance = getDistance();
+    
+    bool irDetected = isObjectDetected();
 
+    String state;
+
+    if(irDetected) {
+
+        state = "ALERT";
+    }else {
+        state = "SAFE";
+    }
+
+    drawDashboard(
+        distance, 
+        irDetected,
+        state
+    );
+
+    delay(200);
+}
 
